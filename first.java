4c7041904fc8398f4fb938a854ca70d74d56c3ca
Sandeep Kartik <sandeepkartik17@gmail.com>
2025-06-04 15:25:29 +0530

    Removed sensative data

---
+++ first.java
@@ -0,0 +1,14 @@
+package mypkg;
+import java.lang.*;
+import java.util.Scanner;
+class first{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter any  first no.  :");
+		int a=sc.nextInt();
+		System.out.println("Enter sec. no. :");
+		int b=sc.nextInt();
+		int z=(a+b);
+		System.out.println("Addition of two no   :" (a+b))    ;
+	}
+}
